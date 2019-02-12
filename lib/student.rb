7583a65a2f0b06c1d9728eebe5083cf9b195ded7
Amil Masic <amil.masic@gmail.com>
2019-02-12 19:53:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
     # binding.pry
      @name = student_hash[:name]
      @location = student_hash[:location]
-     @all << self.name
+     self.all << self
   end
 
   def self.create_from_collection(students_array)
