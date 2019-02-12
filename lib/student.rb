ecaece96578dc93191d4c24baa3e929cf8473b79
Amil Masic <amil.masic@gmail.com>
2019-02-12 20:08:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,9 +23,10 @@ class Student
 
   def self.create_from_collection(students_array)
     binding.pry
-    students = []
-    students_array.collect do |newStudent|
-      student = Student.new(newStudent)
+
+    students_array.each do |newStudent|
+      Student.new(newStudent)
+    end
   end
 
   def add_student_attributes(attributes_hash)
