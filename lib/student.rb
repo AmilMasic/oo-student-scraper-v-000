09c82bcbe2da8b73aa4f8757ce0692b27ee37fec
Amil Masic <amil.masic@gmail.com>
2019-02-12 20:10:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,24 +6,13 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    # @name = name
-    # @location = location
-    # @twitter = twitter
-    # @linkedin = linkedin
-    # @github = github
-    # @blog = blog
-    # @profile_quote = profile_quote
-    # @bio = bio
-    # @profile_url = profile_url
-    # binding.pry
+
      @name = student_hash[:name]
      @location = student_hash[:location]
      @@all << self
   end
 
   def self.create_from_collection(students_array)
-    # binding.pry
-
     students_array.each do |newStudent|
       Student.new(newStudent)
     end
