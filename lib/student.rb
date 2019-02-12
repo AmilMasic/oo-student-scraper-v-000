941c06cb6225ba53d31f217da3c03c6c22acca46
Amil Masic <amil.masic@gmail.com>
2019-02-12 19:50:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,11 +14,12 @@ class Student
     # @profile_quote = profile_quote
     # @bio = bio
     # @profile_url = profile_url
-    puts student_hash
+    binding.pru
+     student_hash
   end
 
   def self.create_from_collection(students_array)
-    
+
   end
 
   def add_student_attributes(attributes_hash)
